56d2b9056a4ee748f8b851a6e07e9962e952e4fb
Muhammad kashif <126161777+MkashifShahid@users.noreply.github.com>
2023-05-16 10:48:49 +0500

    Add files via upload

---
+++ 01.Sum of Two numbers.c++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"please Enter First collection "<<endl;
+int amount1;
+cin>>amount1;
+
+ cout<<"please Enter Second collection "<<endl;
+int amount2;
+cin>>amount2;
+
+//sum of both collection
+
+int sum=amount1+amount2;
+cout<<"The Sum of Both Collections=   "<<sum;
+return 0;
+}
